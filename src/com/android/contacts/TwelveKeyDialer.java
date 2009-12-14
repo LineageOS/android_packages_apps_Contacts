69bd8e06f56ceb163197036433a3501f50ce6372
Wysie <sohyuanchin@gmail.com>
2009-12-14 20:19:50 +0800

    Added new drawables for SMS/MMS. Release 1.1.

--- src/com/android/contacts/TwelveKeyDialer.java
+++ src/com/android/contacts/TwelveKeyDialer.java
@@ -490,7 +490,7 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         mAddToContactMenuItem = menu.add(0, 0, 0, R.string.recentCalls_addToContact).setIcon(android.R.drawable.ic_menu_add);
-        mSmsMenuItem = menu.add(0, 1, 0, "SMS / MMS").setIcon(R.drawable.badge_action_sms);
+        mSmsMenuItem = menu.add(0, 1, 0, "SMS / MMS").setIcon(R.drawable.ic_menu_smsmms);
 	mPreferences = menu.add(0, 1, 0, "Preferences").setIcon(android.R.drawable.ic_menu_preferences);
 
         return true;
@@ -1074,7 +1074,7 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
     	        }
     	}
     	else if (leftButtonType.equals(DialerSettings.SMS)) {
-    		mVoicemailButton.setImageResource(R.drawable.badge_action_sms);
+    		mVoicemailButton.setImageResource(R.drawable.ic_dial_action_sms);
     	}
     }
     

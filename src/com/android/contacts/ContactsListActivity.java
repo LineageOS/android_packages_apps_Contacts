5d5d648edd9b16c92e9188f0392f3032f8f9a2d8
David Kohen <kohen.d@gmail.com>
2010-07-08 23:58:32 +0300

    Make my last change compile, you should always compile after comments...

--- src/com/android/contacts/ContactsListActivity.java
+++ src/com/android/contacts/ContactsListActivity.java
@@ -3554,8 +3554,8 @@ public class ContactsListActivity extends ListActivity implements View.OnCreateC
                         mIndexer = getNewIndexer(cursor);
                     }
                 }
-            }
             */
+            }
             Bundle bundle = cursor.getExtras();
             if (bundle.containsKey(ContactCounts.EXTRA_ADDRESS_BOOK_INDEX_TITLES)) {
                 String sections[] =

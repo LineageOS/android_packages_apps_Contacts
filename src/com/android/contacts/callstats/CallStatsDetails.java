f7967e7b52a5c3717b3d8fe528e875fc12fd8179
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-18 14:33:01 +0100

    Add Call statistics to Contacts (Phone)
    Available from the local menu in the Phone application (except from
    the Dialer Fragment).
    
    Call-type filter can be selected in the actionbar.
    If all call types are selected the incoming/outgoing rates show up in
    blue/green respectively.
    Tapping on an item starts a new activity that shows all call types with
    details for the given contact or number.
    
    Sample screenshots: http://files.sethyx.info/aokp/call_stats_sample.png
    
    PS1: Import from AOKP
    PS2: Cleanup, avoid code duplication, avoid broken call log for
         non-English locales
    PS3: Add German translation, cleanup time formatting
    PS4: Fix counting issues in header when not displaying all calls
    PS5: Improve layout and duration formatting
    PS6: Tweak details activity layout
    PS7: Show number of incoming/outgoing calls in details activity.
    PS8: Allow sorting by date, move filter reset out of date picker,
         prevent invalid date ranges, cleanup
    PS9: Fix derp
    PS10: Reduce code duplication
    PS11: Whitespace fixes, remove now unneeded drawables
    PS12: Final cleanup
    
    Change-Id: I94cd49e167b82bc7eaeab9225ed96a273a33b18c

---
+++ src/com/android/contacts/callstats/CallStatsDetails.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.contacts.callstats;
+
+import android.content.Intent;
+import android.content.Context;
+import android.net.Uri;
+import android.provider.CallLog.Calls;
+import android.util.Log;
+
+import com.android.contacts.CallDetailHeader;;
+import com.android.contacts.util.UriUtils;
+
+/**
+ * Class to store statistical details for a given contact/number.
+ */
+public class CallStatsDetails implements CallDetailHeader.Data {
+
+    private static final String TAG = "CallStatsDetails";
+    private static final String NUMBER = "number";
+    private static final String F_NUMBER = "f_number";
+    private static final String C_ISO = "c_iso";
+    private static final String GEOCODE = "geocode";
+    private static final String NAME = "name";
+    private static final String DATE = "date";
+    private static final String NUM_TYPE = "num_type";
+    private static final String NUM_LABEL = "num_label";
+    private static final String C_URI = "c_uri";
+    private static final String PHOTO_ID = "photo_id";
+    private static final String PHOTO_URI = "photo_uri";
+    private static final String IN_DUR = "in_dur";
+    private static final String OUT_DUR = "out_dur";
+    private static final String INCOMING = "incoming";
+    private static final String OUTGOING = "outgoing";
+    private static final String MISSED = "missed";
+
+    public final CharSequence number;
+    public final CharSequence formattedNumber;
+    public final String countryIso;
+    public final String geocode;
+    public final long date;
+    public final CharSequence name;
+    public final int numberType;
+    public final CharSequence numberLabel;
+    public Uri contactUri;
+    public Uri photoUri;
+    public final long photoId;
+    public long inDuration;
+    public long outDuration;
+    public int incomingCount;
+    public int outgoingCount;
+    public int missedCount;
+
+    /** Used when no contact has been found for this number */
+    public CallStatsDetails(CharSequence number, CharSequence formattedNumber,
+            String countryIso, String geocode, long date) {
+        this(number, formattedNumber, countryIso, geocode, date, "", 0, "",
+                null, null, 0, 0, 0, 0, 0, 0);
+    }
+
+    /**
+     * Used when both contact information and initial call statistics are
+     * available
+     */
+    public CallStatsDetails(CharSequence number, CharSequence formattedNumber,
+            String countryIso, String geocode, long date, CharSequence name,
+            int numberType, CharSequence numberLabel, Uri contactUri,
+            Uri photoUri, long photoId, long inDuration, long outDuration,
+            int incomingCount, int outgoingCount, int missedCount) {
+        this.number = number;
+        this.formattedNumber = formattedNumber;
+        this.countryIso = countryIso;
+        this.geocode = geocode;
+        this.date = date;
+        this.name = name;
+        this.numberType = numberType;
+        this.numberLabel = numberLabel;
+        this.contactUri = contactUri;
+        this.photoUri = photoUri;
+        this.photoId = photoId;
+        this.inDuration = inDuration;
+        this.outDuration = outDuration;
+        this.incomingCount = incomingCount;
+        this.outgoingCount = outgoingCount;
+        this.missedCount = missedCount;
+    }
+
+    /** Used when we have a contact, but no statistics */
+    public CallStatsDetails(CharSequence number, CharSequence formattedNumber,
+            String countryIso, String geocode, long date, CharSequence name,
+            int numberType, CharSequence numberLabel, Uri contactUri,
+            Uri photoUri, long photoId) {
+        this(number, formattedNumber, countryIso, geocode, date, name,
+                numberType, numberLabel, contactUri, photoUri, photoId, 0, 0, 0, 0, 0);
+    }
+
+    @Override
+    public CharSequence getName() {
+        return name;
+    }
+    @Override
+    public CharSequence getNumber() {
+        return number;
+    }
+    @Override
+    public int getNumberType() {
+        return numberType;
+    }
+    @Override
+    public CharSequence getNumberLabel() {
+        return numberLabel;
+    }
+    @Override
+    public CharSequence getFormattedNumber() {
+        return formattedNumber;
+    }
+    @Override
+    public Uri getContactUri() {
+        return contactUri;
+    }
+
+    public long getFullDuration() {
+        return inDuration + outDuration;
+    }
+
+    public int getTotalCount() {
+        return incomingCount + outgoingCount + missedCount;
+    }
+
+    public void addTimeOrMissed(int type, long time) {
+        switch (type) {
+            case Calls.INCOMING_TYPE:
+                incomingCount++;
+                inDuration += time;
+                break;
+            case Calls.OUTGOING_TYPE:
+                outgoingCount++;
+                outDuration += time;
+                break;
+            case Calls.MISSED_TYPE:
+                missedCount++;
+                break;
+        }
+    }
+
+    public int getDurationPercentage(int type) {
+        long duration = getRequestedDuration(type);
+        return Math.round((float) duration * 100F / getFullDuration());
+    }
+
+    public int getCountPercentage(int type) {
+        int count = getRequestedCount(type);
+        return Math.round((float) count * 100F / getTotalCount());
+    }
+
+    /** Used to generate an intent to later recreate this item */
+    public Intent getIntentWithExtras(Context context) {
+        Intent intent = new Intent(context, CallStatsDetailActivity.class);
+        intent.putExtra(NUMBER, number);
+        intent.putExtra(F_NUMBER, formattedNumber);
+        intent.putExtra(C_ISO, countryIso);
+        intent.putExtra(GEOCODE, geocode);
+        intent.putExtra(DATE, date);
+        intent.putExtra(NAME, name);
+        intent.putExtra(NUM_TYPE, numberType);
+        intent.putExtra(NUM_LABEL, numberLabel);
+        intent.putExtra(PHOTO_ID, photoId);
+        intent.putExtra(IN_DUR, inDuration);
+        intent.putExtra(OUT_DUR, outDuration);
+        intent.putExtra(INCOMING, incomingCount);
+        intent.putExtra(OUTGOING, outgoingCount);
+        intent.putExtra(MISSED, missedCount);
+        try {
+            intent.putExtra(C_URI, contactUri.toString());
+        } catch (NullPointerException ex) {
+            Log.i(TAG, "Could not add contact uri");
+        }
+        try {
+            intent.putExtra(PHOTO_URI, photoUri.toString());
+        } catch (NullPointerException ex) {
+            Log.i(TAG, "Could not add photo uri");
+        }
+        return intent;
+    }
+
+    /** Used to recreate an object from an intent */
+    public static CallStatsDetails reCreateFromIntent(Intent intent) {
+        final String number = intent.getStringExtra(NUMBER);
+        final String formattedNumber = intent.getStringExtra(F_NUMBER);
+        final String countryIso = intent.getStringExtra(C_ISO);
+        final String geocode = intent.getStringExtra(GEOCODE);
+        final long date = intent.getLongExtra(DATE, 0);
+        final String name = intent.getStringExtra(NAME);
+        final int numType = intent.getIntExtra(NUM_TYPE, 0);
+        final String numLabel = intent.getStringExtra(NUM_LABEL);
+        final Uri contacturi = UriUtils.parseUriOrNull(intent.getStringExtra(C_URI));
+        final Uri photouri = UriUtils.parseUriOrNull(intent.getStringExtra(PHOTO_URI));
+        final long photoid = intent.getLongExtra(PHOTO_ID, 0);
+        final long inDuration = intent.getLongExtra(IN_DUR, 0);
+        final long outDuration = intent.getLongExtra(OUT_DUR, 0);
+        final int incoming = intent.getIntExtra(INCOMING, 0);
+        final int outgoing = intent.getIntExtra(OUTGOING, 0);
+        final int missed = intent.getIntExtra(MISSED, 0);
+
+        return new CallStatsDetails(number, formattedNumber, countryIso, geocode, date,
+                name, numType, numLabel, contacturi, photouri, photoid,
+                inDuration, outDuration, incoming, outgoing, missed);
+    }
+
+    public long getRequestedDuration(int type) {
+        switch (type) {
+            case Calls.INCOMING_TYPE:
+                return inDuration;
+            case Calls.OUTGOING_TYPE:
+                return outDuration;
+            case Calls.MISSED_TYPE:
+                return (long) missedCount;
+            default:
+                return getFullDuration();
+        }
+    }
+
+    public int getRequestedCount(int type) {
+        switch (type) {
+            case Calls.INCOMING_TYPE:
+                return incomingCount;
+            case Calls.OUTGOING_TYPE:
+                return outgoingCount;
+            case Calls.MISSED_TYPE:
+                return missedCount;
+            default:
+                return getTotalCount();
+        }
+    }
+    public void mergeWith(CallStatsDetails other) {
+        this.inDuration += other.inDuration;
+        this.outDuration += other.outDuration;
+        this.incomingCount += other.incomingCount;
+        this.outgoingCount += other.outgoingCount;
+        this.missedCount += other.missedCount;
+    }
+}
---
+++ src/com/android/contacts/callstats/DoubleDatePickerDialog.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.contacts.callstats;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
+
+import java.lang.System;
+import java.util.Calendar;
+
+import com.android.contacts.R;
+
+/**
+ * Alertdialog with two date pickers - one for a start and one for an end date.
+ * Used to filter the callstats query.
+ */
+public class DoubleDatePickerDialog extends AlertDialog
+        implements OnClickListener, OnDateChangedListener {
+
+    private static final String TAG = "DoubleDatePickerDialog";
+
+    public interface OnDateSetListener {
+        void onDateSet(long from, long to);
+    }
+
+    private static final String YEAR = "year";
+    private static final String MONTH = "month";
+    private static final String DAY = "day";
+
+    private final DatePicker mDatePickerFrom;
+    private final DatePicker mDatePickerTo;
+    private final OnDateSetListener mCallBack;
+    private Button mOkButton;
+
+    public DoubleDatePickerDialog(Context context,
+            OnDateSetListener callBack) {
+        super(context);
+
+        mCallBack = callBack;
+
+        setTitle(R.string.call_stats_filter_picker_title);
+        setButton(BUTTON_NEGATIVE, context.getString(android.R.string.cancel), this);
+        setButton(BUTTON_POSITIVE, context.getString(android.R.string.ok), this);
+        setIcon(0);
+
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.double_date_picker_dialog, null);
+        setView(view);
+
+        mDatePickerFrom = (DatePicker) view.findViewById(R.id.datePickerFrom);
+        mDatePickerTo = (DatePicker) view.findViewById(R.id.datePickerTo);
+        resetPickers();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mOkButton = getButton(DialogInterface.BUTTON_POSITIVE);
+        updateOkButtonState();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case BUTTON_POSITIVE:
+                tryNotifyDateSet();
+                break;
+            case BUTTON_NEGATIVE:
+                break;
+        }
+    }
+
+    public void onDateChanged(DatePicker view, int year,
+            int month, int day) {
+        view.init(year, month, day, this);
+        updateOkButtonState();
+    }
+
+    public void setValues(long millisFrom, long millisTo) {
+        setPicker(mDatePickerFrom, millisFrom);
+        setPicker(mDatePickerTo, millisTo);
+        updateOkButtonState();
+    }
+
+    public void resetPickers() {
+        long millis = System.currentTimeMillis();
+        setPicker(mDatePickerFrom, millis);
+        setPicker(mDatePickerTo, millis);
+        updateOkButtonState();
+    }
+
+    private void setPicker(DatePicker picker, long millis) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(millis);
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        picker.init(year, month, day, this);
+    }
+
+    private long getMillisForPicker(DatePicker picker, boolean endOfDay) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, picker.getYear());
+        c.set(Calendar.MONTH, picker.getMonth());
+        c.set(Calendar.DAY_OF_MONTH, picker.getDayOfMonth());
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+
+        long millis = c.getTimeInMillis();
+        if (endOfDay) {
+            millis += 24L * 60L * 60L * 1000L - 1L;
+        }
+
+        return millis;
+    }
+
+    private void updateOkButtonState() {
+        if (mOkButton != null) {
+            long millisFrom = getMillisForPicker(mDatePickerFrom, false);
+            long millisTo = getMillisForPicker(mDatePickerTo, true);
+            mOkButton.setEnabled(millisFrom < millisTo);
+        }
+    }
+
+    private void tryNotifyDateSet() {
+        if (mCallBack != null) {
+            mDatePickerFrom.clearFocus();
+            mDatePickerTo.clearFocus();
+
+            long millisFrom = getMillisForPicker(mDatePickerFrom, false);
+            long millisTo = getMillisForPicker(mDatePickerTo, true);
+
+            mCallBack.onDateSet(millisFrom, millisTo);
+        }
+    }
+
+    // users like to play with it, so save the state and don't reset each time
+    @Override
+    public Bundle onSaveInstanceState() {
+        Bundle state = super.onSaveInstanceState();
+        state.putInt("F_" + YEAR, mDatePickerFrom.getYear());
+        state.putInt("F_" + MONTH, mDatePickerFrom.getMonth());
+        state.putInt("F_" + DAY, mDatePickerFrom.getDayOfMonth());
+        state.putInt("T_" + YEAR, mDatePickerTo.getYear());
+        state.putInt("T_" + MONTH, mDatePickerTo.getMonth());
+        state.putInt("T_" + DAY, mDatePickerTo.getDayOfMonth());
+        return state;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int fyear = savedInstanceState.getInt("F_" + YEAR);
+        int fmonth = savedInstanceState.getInt("F_" + MONTH);
+        int fday = savedInstanceState.getInt("F_" + DAY);
+        int tyear = savedInstanceState.getInt("T_" + YEAR);
+        int tmonth = savedInstanceState.getInt("T_" + MONTH);
+        int tday = savedInstanceState.getInt("T_" + DAY);
+        mDatePickerFrom.init(fyear, fmonth, fday, this);
+        mDatePickerTo.init(tyear, tmonth, tday, this);
+    }
+}

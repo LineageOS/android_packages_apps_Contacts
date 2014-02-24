0aad8f7890d029caf0856aeb3c0a7e86ae241bf5
kaiyiz <kaiyiz@codeaurora.org>
2014-02-24 17:58:57 +0800

    Contacts: can't add member in local group
    Set the wrong intent action when startup
    multi pick contact activity.
    
    Set the right intent action when startup
    multi pick contact activity.
    
    CRs-Fixed: 621087
    
    Change-Id: Iee76307cf955d38751dbed61009d34377b48c890

--- src/com/android/contacts/editor/MultiPickContactActivity.java
+++ src/com/android/contacts/editor/MultiPickContactActivity.java
@@ -237,12 +237,12 @@ public class MultiPickContactActivity extends ListActivity implements
     private static final int MODE_SEARCH_CALL = MODE_DEFAULT_CALL | MODE_MASK_SEARCH;
     private static final int MODE_SEARCH_SIM = MODE_DEFAULT_SIM | MODE_MASK_SEARCH;
 
-    static final String ACTION_MULTI_PICK = "com.android.contacts.action.MULTI_PICK";
+    public static final String ACTION_MULTI_PICK = "com.android.contacts.action.MULTI_PICK";
     static final String ACTION_MULTI_PICK_EMAIL = "com.android.contacts.action.MULTI_PICK_EMAIL";
     static final String ACTION_MULTI_PICK_CALL = "com.android.contacts.action.MULTI_PICK_CALL";
     static final String ACTION_MULTI_PICK_SIM = "com.android.contacts.action.MULTI_PICK_SIM";
 
-    private static final String IS_CONTACT ="is_contact";
+    public static final String IS_CONTACT ="is_contact";
 
     private static final int DIALOG_DEL_CALL = 1;
 

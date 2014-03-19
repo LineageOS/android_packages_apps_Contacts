e261d5e8af3468378f2b58db6421cd771b62aec6
Cheng Xiaolong <chengxiaolong@codeaurora.org>
2014-03-18 22:59:00 -0400

    Fix no response when tap internet call field
    Except for Phone.CONTENT_ITEM_TYPE, using mPrimaryActionClickListener
    to respond the click action for other item type.
    For Phone.CONTENT_ITEM_TYPE, there is other onClickListener to respond
    it.
    
    CRs-Fixed: 633528
    
    Change-Id: Id72170d7b54e6227c86c072b731468884b4d79c5

--- src/com/android/contacts/detail/ContactDetailFragment.java
+++ src/com/android/contacts/detail/ContactDetailFragment.java
@@ -40,8 +40,9 @@ import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Directory;
 import android.provider.ContactsContract.DisplayNameSources;
 import android.provider.ContactsContract.StatusUpdates;
-import android.telephony.TelephonyManager;
 import android.provider.LocalGroups.Group;
+import android.telephony.MSimTelephonyManager;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -1717,9 +1718,14 @@ public class ContactDetailFragment extends Fragment implements FragmentKeyListen
                 v = mInflater.inflate(R.layout.contact_detail_list_item, parent, false);
 
                 // Cache the children
-                viewCache = new DetailViewCache(v,
-                        mPrimaryActionClickListener, mSecondaryActionClickListener,
-                        mThirdActionClickListener);
+                if (MSimTelephonyManager.getDefault().isMultiSimEnabled()
+                        && Phone.CONTENT_ITEM_TYPE.equals(entry.mimetype)) {
+                    viewCache = new DetailViewCache(v, null, mSecondaryActionClickListener,
+                            mThirdActionClickListener);
+                } else {
+                    viewCache = new DetailViewCache(v, mPrimaryActionClickListener,
+                            mSecondaryActionClickListener, mThirdActionClickListener);
+                }
                 v.setTag(viewCache);
             }
 

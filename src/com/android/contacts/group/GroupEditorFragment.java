14bae53757868a7c3e17e920ab4be68fe02eca51
blong <blong@codeaurora.org>
2014-10-08 17:06:11 +0800

    Supports to create group in Phone account
    - Fix the problem that can't create group in phone account
    
    Change-Id: If39a73b35718ca77458beef4bbc0965654da1300
    CRs-Fixed: 730058

--- src/com/android/contacts/group/GroupEditorFragment.java
+++ src/com/android/contacts/group/GroupEditorFragment.java
@@ -311,8 +311,9 @@ public class GroupEditorFragment extends Fragment implements SelectAccountDialog
     }
 
     private void selectAccountAndCreateGroup() {
-        final List<AccountWithDataSet> accounts =
-                AccountTypeManager.getInstance(mContext).getAccounts(true /* writeable */);
+        final List<AccountWithDataSet> accounts = AccountTypeManager
+                .getInstance(mContext).getAccounts(true /* writeable */,
+                        AccountTypeManager.FLAG_ALL_ACCOUNTS_WITHOUT_SIM);
         // No Accounts available
         if (accounts.isEmpty()) {
             Log.e(TAG, "No accounts were found.");

e5e29c71a9b64918c3830d8c000f9dc6a1d51d57
Jay Shrauner <shrauner@google.com>
2014-11-26 10:44:28 -0800

    Fix NPE in shareContact
    Check whether we have a shareable contact before trying to share it.
    
    Bug:18535080
    Change-Id: Id49d40227f58c592ff83c0d848ac690b85dde8d5

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -2658,7 +2658,9 @@ public class QuickContactActivity extends ContactsActivity {
                 deleteContact();
                 return true;
             case R.id.menu_share:
-                shareContact();
+                if (isContactShareable()) {
+                    shareContact();
+                }
                 return true;
             case R.id.menu_send_via_sms: {
                 if (mContactData == null) {

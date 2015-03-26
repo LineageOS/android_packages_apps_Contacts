2bb707da786a8831618eaef933d863f28d1f05dd
Danny Baumann <dannybaumann@web.de>
2015-03-26 13:22:39 +0100

    Don't show default and favorites groups in group overview.
    Change-Id: I37a0c7101f6bb24706e88b0128e466101bf89cc1

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -1955,8 +1955,15 @@ public class QuickContactActivity extends ContactsActivity {
     private Entry groupDataItemsToEntry(List<DataItem> dataItems) {
         final List<String> titles = new ArrayList<>();
         for (DataItem dataItem : dataItems) {
-            final String title = dataItem instanceof GroupMembershipDataItem
-                    ? ((GroupMembershipDataItem) dataItem).getGroupTitle() : null;
+            if (!(dataItem instanceof GroupMembershipDataItem)) {
+                continue;
+            }
+
+            final GroupMembershipDataItem groupItem = (GroupMembershipDataItem) dataItem;
+            if (groupItem.isDefaultGroup() || groupItem.isFavoritesGroup()) {
+                continue;
+            }
+            final String title = groupItem.getGroupTitle();
             if (title != null) {
                 titles.add(title);
             }

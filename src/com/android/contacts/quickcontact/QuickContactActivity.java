37287451a4aa187fa5d8f3391c25158bd2c4c180
cretin45 <cretin45@gmail.com>
2015-09-24 16:08:44 -0700

    Contacts: Save contact image from lookup
    Requires: http://review.cyanogenmod.org/#/c/110252/
    
    Addresses MMS-149
    
    Change-Id: Iaf0f3a2e87b19391869eb812a3bb3945d63b8621

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -141,6 +141,7 @@ import com.android.contacts.common.model.dataitem.WebsiteDataItem;
 import com.android.contacts.common.util.ImplicitIntentsUtil;
 import com.android.contacts.common.MoreContactUtils;
 import com.android.contacts.common.SimContactsConstants;
+import com.android.contacts.common.util.BitmapUtil;
 import com.android.contacts.common.util.DateUtils;
 import com.android.contacts.common.util.MaterialColorMapUtils;
 import com.android.contacts.common.util.MaterialColorMapUtils.MaterialPalette;
@@ -172,6 +173,7 @@ import com.android.contactsbind.HelpUtils;
 import com.google.common.collect.Lists;
 import com.google.common.collect.ImmutableList;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 import java.lang.SecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -279,6 +281,8 @@ public class QuickContactActivity extends ContactsActivity {
     private int mColorFilterColor;
 
     private final ImageViewDrawableSetter mPhotoSetter = new ImageViewDrawableSetter();
+
+    private Target mContactBitmapTarget;
 
     /**
      * {@link #LEADING_MIMETYPES} is used to sort MIME-types.
@@ -1233,12 +1237,24 @@ public class QuickContactActivity extends ContactsActivity {
 
         mPhotoView.setIsBusiness(mContactData.isDisplayNameFromOrganization());
         if (mContactData.getPhotoBinaryData() == null && mContactData.getPhotoUri() != null) {
-            Picasso.with(getApplicationContext())
-                    .load(mContactData.getPhotoUri())
-                    .noPlaceholder()
-                    .centerCrop()
-                    .resize(480, 640) // Just a reasonable default
-                    .into(mPhotoView);
+            mContactBitmapTarget = new Target() {
+                @Override
+                public void onPrepareLoad(Drawable d){}
+                @Override
+                public void onBitmapLoaded(Bitmap result, Picasso.LoadedFrom from) {
+                    if (result != null) {
+                        mContactData.setPhotoBinaryData(BitmapUtil.bitmapToByteArray(result));
+                        mPhotoSetter.setupContactPhoto(data, mPhotoView);
+                    }
+                    mContactBitmapTarget = null;
+                }
+                @Override
+                public void onBitmapFailed(Drawable drawable) {
+                    mPhotoSetter.setupContactPhoto(data, mPhotoView);
+                    mContactBitmapTarget = null;
+                }
+            };
+            Picasso.with(this).load(mContactData.getPhotoUri()).into(mContactBitmapTarget);
         } else {
             mPhotoSetter.setupContactPhoto(data, mPhotoView);
         }

5391aedac2e13155f95e667918ae0956f4d12261
Artem Shvadskiy <ashvadskiy@cyngn.com>
2016-05-23 14:54:22 -0700

    Request storage permission for gallery access
    Change-Id: Icd95df068e27096eab4759b99f73fa4ce4ca1dde
    issue-id: FEIJ-238

--- src/com/android/contacts/activities/CompactContactEditorActivity.java
+++ src/com/android/contacts/activities/CompactContactEditorActivity.java
@@ -16,6 +16,7 @@
 
 package com.android.contacts.activities;
 
+import android.app.Fragment;
 import com.android.contacts.R;
 import com.android.contacts.editor.CompactContactEditorFragment;
 import com.android.contacts.common.activity.RequestPermissionsActivity;
@@ -61,6 +62,17 @@ public class CompactContactEditorActivity extends ContactEditorBaseActivity {
     public void onBackPressed() {
         if (mFragment != null) {
             mFragment.revert();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (mFragment != null) {
+            ((Fragment) mFragment).onRequestPermissionsResult(
+                    requestCode, permissions, grantResults);
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- src/com/android/contacts/activities/ContactEditorActivity.java
+++ src/com/android/contacts/activities/ContactEditorActivity.java
@@ -16,6 +16,7 @@
 
 package com.android.contacts.activities;
 
+import android.app.Fragment;
 import com.android.contacts.R;
 import com.android.contacts.common.activity.RequestPermissionsActivity;
 import com.android.contacts.editor.ContactEditorFragment;
@@ -55,6 +56,17 @@ public class ContactEditorActivity extends ContactEditorBaseActivity
     public void onBackPressed() {
         if (mFragment != null) {
             mFragment.save(ContactEditor.SaveMode.COMPACT, /* backPressed =*/ true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (mFragment != null) {
+            ((Fragment) mFragment).onRequestPermissionsResult(
+                    requestCode, permissions, grantResults);
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }

6aebf68021fc09017a8ba16377a2c63817b95a51
Stephen Bird <sbird@cyngn.com>
2016-07-12 14:47:27 -0700

    [2/4] Add cancel callback for BlockContactDialog
    Some ui elements need to refresh themselves when the dialog
    is cancelled. This allows them to do that.
    
    Change-Id: Iffa1861da93b4e070ace0168c3bccafcdf07b5a7
    Ticket: CYNGNOS-3111

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -535,6 +535,11 @@ public class QuickContactActivity extends ContactsActivity implements
     };
 
     @Override
+    public void onBlockCancelled() {
+        // Do nothing
+    }
+
+    @Override
     public void onBlockSelected(boolean notifyLookupProvider) {
         mBlockContactHelper.blockContactAsync(notifyLookupProvider);
     }

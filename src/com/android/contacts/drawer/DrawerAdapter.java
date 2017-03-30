a4e8ed26ff34cd0d63983fdf388620d056ff2389
yuichiro fujiwara <yuichiro.fujiwara@sony.com>
2017-03-30 17:39:02 +0900

    Place EmergencyInfo shortcut on drawer
    Add EmegencyInfo shortcut on drawer list.
    User can launch from the navigation drawer easily.
    
    Bug: 38188696
    Change-Id: I0c6a4b76cfbf55dfe3d08867dc1313739344ee49

--- src/com/android/contacts/drawer/DrawerAdapter.java
+++ src/com/android/contacts/drawer/DrawerAdapter.java
@@ -36,6 +36,7 @@ import com.android.contacts.list.ContactListFilter;
 import com.android.contacts.model.account.AccountDisplayInfo;
 import com.android.contacts.model.account.AccountDisplayInfoFactory;
 import com.android.contacts.profile.ProfileItem;
+import com.android.contacts.util.ImplicitIntentsUtil;
 import com.android.contacts.util.SharedPreferenceUtil;
 import com.android.contactsbind.ObjectFactory;
 
@@ -54,9 +55,10 @@ public class DrawerAdapter extends BaseAdapter {
     private static final int VIEW_TYPE_STATUS_SPACER = 7;
     private static final int VIEW_TYPE_NAV_DIVIDER = 8;
     private static final int VIEW_TYPE_PROFILE_ENTRY = 9;
+    private static final int VIEW_TYPE_EMERGENCY_ITEM = 10;
 
     // This count must be updated if we add more view types.
-    private static final int VIEW_TYPE_COUNT = 10;
+    private static final int VIEW_TYPE_COUNT = 11;
 
     private static final int TYPEFACE_STYLE_ACTIVATE = R.style.DrawerItemTextActiveStyle;
     private static final int TYPEFACE_STYLE_INACTIVE = R.style.DrawerItemTextInactiveStyle;
@@ -80,6 +82,8 @@ public class DrawerAdapter extends BaseAdapter {
     //  [Create Label button]
     //  [Account Header]
     //  [Accounts]
+    //  [Emergency information]
+    //  [Emergency information spacer item]
     //  [Misc items] (a divider, Settings, Help & Feedback)
     //  [Navigation spacer item]
     private StatusBarSpacerItem mStatusBarSpacerItem = null;
@@ -92,6 +96,8 @@ public class DrawerAdapter extends BaseAdapter {
     private BaseDrawerItem mCreateLabelButton = null;
     private HeaderItem mAccountHeader = null;
     private List<AccountEntryItem> mAccountEntries = new ArrayList<>();
+    private BaseDrawerItem mEmergencyItem = null;
+    private DividerItem mEmergencyItemDivider = null;
     private List<BaseDrawerItem> mMiscItems = new ArrayList<>();
 
     private List<BaseDrawerItem> mItemsList = new ArrayList<>();
@@ -123,6 +129,11 @@ public class DrawerAdapter extends BaseAdapter {
         // Create Label Button
         mCreateLabelButton = new BaseDrawerItem(VIEW_TYPE_CREATE_LABEL, R.id.nav_create_label,
                 R.string.menu_new_group_action_bar, R.drawable.quantum_ic_add_vd_theme_24);
+        // Emergency information Item
+        mEmergencyItem = new BaseDrawerItem(VIEW_TYPE_EMERGENCY_ITEM, R.id.nav_emergency,
+                R.string.menu_emergency_information_txt,
+                R.drawable.quantum_ic_drawer_emergency_info_24);
+        mEmergencyItemDivider = new DividerItem();
         // Misc Items
         mMiscItems.add(new DividerItem());
         mMiscItems.add(new MiscItem(R.id.nav_settings, R.string.menu_settings,
@@ -152,6 +163,10 @@ public class DrawerAdapter extends BaseAdapter {
             mItemsList.add(mAccountHeader);
         }
         mItemsList.addAll(mAccountEntries);
+        if (ImplicitIntentsUtil.getIntentForEmergencyInfo(mActivity) != null) {
+            mItemsList.add(mEmergencyItemDivider);
+            mItemsList.add(mEmergencyItem);
+        }
         mItemsList.addAll(mMiscItems);
         mItemsList.add(mNavSpacerItem);
     }
@@ -228,6 +243,8 @@ public class DrawerAdapter extends BaseAdapter {
                 return getBaseItemView(R.layout.nav_drawer_spacer, view, viewGroup);
             case VIEW_TYPE_NAV_DIVIDER:
                 return getBaseItemView(R.layout.drawer_horizontal_divider, view, viewGroup);
+            case VIEW_TYPE_EMERGENCY_ITEM:
+                return getDrawerItemView(drawerItem, view, viewGroup);
         }
         throw new IllegalStateException("Unknown drawer item " + drawerItem);
     }

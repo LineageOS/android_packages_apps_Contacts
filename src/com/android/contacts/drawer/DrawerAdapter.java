d1c544e4b1297a2cbc7a83d2de20bcca6e70630a
John Shao <johnshao@google.com>
2017-11-07 21:07:18 -0800

    Hide help and feedback in AOSP contacts
    Change-Id: I8daf9ef8ebc99254cadab5f914db2f268f2422e3

--- src/com/android/contacts/drawer/DrawerAdapter.java
+++ src/com/android/contacts/drawer/DrawerAdapter.java
@@ -38,6 +38,7 @@ import com.android.contacts.model.account.AccountDisplayInfoFactory;
 import com.android.contacts.profile.ProfileItem;
 import com.android.contacts.util.ImplicitIntentsUtil;
 import com.android.contacts.util.SharedPreferenceUtil;
+import com.android.contactsbind.HelpUtils;
 import com.android.contactsbind.ObjectFactory;
 
 import java.util.ArrayList;
@@ -138,8 +139,10 @@ public class DrawerAdapter extends BaseAdapter {
         mMiscItems.add(new DividerItem());
         mMiscItems.add(new MiscItem(R.id.nav_settings, R.string.menu_settings,
                 R.drawable.quantum_ic_settings_vd_theme_24));
-        mMiscItems.add(new MiscItem(R.id.nav_help, R.string.menu_help,
-                R.drawable.quantum_ic_help_vd_theme_24));
+        if (HelpUtils.isHelpAndFeedbackAvailable()) {
+            mMiscItems.add(new MiscItem(R.id.nav_help, R.string.menu_help,
+                    R.drawable.quantum_ic_help_vd_theme_24));
+        }
         rebuildItemsList();
     }
 

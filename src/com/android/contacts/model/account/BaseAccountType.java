b1cff32162c93941b1a101b11a2eb7873a86a326
Asher Simonds <dayanhammer@gmail.com>
2018-02-12 10:23:45 +0100

    Contacts: adaptive icon
    Change-Id: Ibacea76eb0f6fdbfacd04bdb82649fe0b9d78f6f
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/contacts/model/account/BaseAccountType.java
+++ src/com/android/contacts/model/account/BaseAccountType.java
@@ -121,7 +121,7 @@ public abstract class BaseAccountType extends AccountType {
         this.accountType = null;
         this.dataSet = null;
         this.titleRes = R.string.account_phone;
-        this.iconRes = R.mipmap.ic_contacts_launcher;
+        this.iconRes = R.mipmap.ic_launcher;
     }
 
     protected static EditType buildPhoneType(int type) {
